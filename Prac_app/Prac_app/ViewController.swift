fdf6e1a184b6ca31a4fab1dcfdc377b80c2423f2
wjdtngh950 <wjdtngh950@naver.com>
2022-03-12 15:16:10 +0900

    프로젝트 추가

---
+++ Prac_app/Prac_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prac_app
+//
+//  Created by 정수호 on 12/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
