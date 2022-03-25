878d57af311925c1372c5b693af9c7b9c590e6b1
Jeong_suho <wjdtngh950@naver.com>
2022-03-25 22:18:45 +0900

    Treatment

---
+++ Language_Practice/Language_Basic.playground/Pages/Treatment.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let sum: Int
+let input1: Int = 100
+var input2: Int = 200
+
+sum = input1 + input2
+input2 = 300
+
+
+//: [Next](@next)
--- Language_Practice/Language_Basic.playground/Pages/상수와 변수.xcplaygroundpage/Contents.swift
+++ Language_Practice/Language_Basic.playground/Pages/상수와 변수.xcplaygroundpage/Contents.swift
@@ -9,5 +9,8 @@ b = 100
 sum = a + b
 print(sum)
 
+let c = 200
+var suhoName = "수호"
 
+print(suhoName)
 //: [Next](@next)
