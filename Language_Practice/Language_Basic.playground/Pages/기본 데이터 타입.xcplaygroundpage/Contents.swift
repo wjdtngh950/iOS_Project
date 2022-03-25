3af1cd5d3e8b5dfc13318ecd0df5a6bb50231b9d
Jeong_suho <wjdtngh950@naver.com>
2022-03-25 18:58:27 +0900

    practice

---
+++ Language_Practice/Language_Basic.playground/Pages/기본 데이터 타입.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+// Int
+var someInt: Int = -100
+someInt = 100
+
+//UInt
+var someUInt: UInt = 200
+
+//Float
+var someFloat: Float = 1.1
+someFloat = 1
+print(someFloat)
+
+//Double
+var someDouble: Double = 1.1
+
+
+//Bool
+var someBool: Bool = true
+someBool = false
+
+//Character
+
+var someCharacter: Character = "가"
+someCharacter = "A"
+someCharacter = "🤔"
+
+// String
+
+var someString: String = "나는 수호입니다"
+someString = "🤔🧑‍💻"
+
+//타입 추론
+var number = 10
+
+//: [Next](@next)
--- Language_Practice/Language_Basic.playground/Pages/상수와 변수.xcplaygroundpage/Contents.swift
+++ Language_Practice/Language_Basic.playground/Pages/상수와 변수.xcplaygroundpage/Contents.swift
@@ -7,6 +7,7 @@ let a: Int = 200
 var b: Int = 300
 b = 100
 sum = a + b
-
 print(sum)
+
+
 //: [Next](@next)
