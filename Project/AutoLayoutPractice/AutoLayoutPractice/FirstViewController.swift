d161c837943f929dc89b29e52fd695ef93faeb2a
Jeong_suho <wjdtngh950@naver.com>
2022-04-05 13:17:42 +0900

    AutoLayout

---
+++ Project/AutoLayoutPractice/AutoLayoutPractice/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by 정수호 on 2022/04/05.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
