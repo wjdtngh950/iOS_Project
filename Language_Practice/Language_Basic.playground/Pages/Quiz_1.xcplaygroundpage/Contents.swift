d2af12dd7dbb8bb95f7e6c1c83d21a0f9613b1d4
Jeong_suho <wjdtngh950@naver.com>
2022-04-08 00:47:16 +0900

    Quiz

--- Language_Practice/Language_Basic.playground/Pages/Quiz_1.xcplaygroundpage/Contents.swift
+++ Language_Practice/Language_Basic.playground/Pages/Quiz_1.xcplaygroundpage/Contents.swift
@@ -64,7 +64,7 @@ getAverage()
 */
  
 
-/*5번 -> 아얘 모르겠음 
+/*5번 -> 아얘 모르겠음
  학생의 이름과 학번이 작성된 출석부(studentList)가 있습니다.
 교실에는 5명의 학생이 있고, 해당학생이 우리반 학생인지 확인하려고 합니다.
 우리반 학생이라면 true를, 아니라면 false를 리턴하는 함수를 작성하세요.*/
@@ -79,10 +79,10 @@ var studentList: [String : Int] = [
 
 var Student: String = "고길동"
 /*(5-1번) + 우리 반에 전학생이 들어왔습니다. 이름은 "최수영", 학번은 101006 입니다. 출석부에 추가해주세요
-studentList.append("최수영" : 102206)
+studentList.append("최수영" : 101006)
 print(studentList)
  (5-2번)  + "홍길동"이 전학을 가게 되었어요. 출석부에서 "홍길동"을 지워주세요!
-studentList.remove("홍길동" : 101001)
+studentList.remove(0)
 (5-3)번 + "김김이"의 학번을 출력해주세요.
 print(studentList[4])*/
 
